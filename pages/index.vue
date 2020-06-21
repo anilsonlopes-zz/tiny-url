888d165d67d0faa3688c45754544951a7a8afd7f
Anilson Lopes <nissobmx@gmail.com>
2020-06-21 03:01:12 -0300

    :rocket: Create document on firebase from a url

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,61 @@
 <template>
-  <div class="container">
+  <div class="mx-auto min-h-screen px-4 flex justify-center items-center">
     <div>
       <logo />
-      <h1 class="title">
+      <h1 class="text-center my-4 text-4xl text-gray-900">
         tiny-url
       </h1>
-      <h2 class="subtitle">
-        Shorten your links with vue and firebase
+      <h2 class="text-center text-lg text-gray-700">
+        Shorten your links with vue/nuxtjs and firebase
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
+      <form class="pt-10" @submit.prevent="generate">
+        <input
+          v-model="url"
+          type="text"
+          placeholder="A long url"
+          class="w-full p-2 border rounded shadow text-center focus:shadow-outline focus:outline-none"
+        />
+        <button
+          type="submit"
+          class="p-2 w-full rounded bg-gray-200 focus:shadow-outline focus:outline-none mt-2"
         >
-          GitHub
-        </a>
-      </div>
+          Generate
+        </button>
+        <div v-if="tiny">
+          <nuxt-link class="tiny" :to="{ name: 'id', params: { id: tiny } }">
+            {{ tiny }}
+          </nuxt-link>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+import firebase from '~/firebase'
 import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
     Logo
+  },
+  data() {
+    return {
+      url: '',
+      tiny: ''
+    }
+  },
+  methods: {
+    async generate() {
+      const doc = await firebase
+        .firestore()
+        .collection('tiny-url')
+        .add({
+          url: this.url,
+          createAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+      this.tiny = doc.id
+    }
   }
 }
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
